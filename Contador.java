71173bb346fa2821738c3d3f1c9b9390923121d5
Vinicius Nascimento <vinninascimento1305@gmail.com>
2023-10-18 17:35:29 -0300

    Controle de Fluxo

---
+++ Contador.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException exception) {
+            System.out.println("Erro: " + exception.getMessage());
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro.");
+        }
+
+        for (int i = 1; i <= (parametroDois - parametroUm); i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
+
+class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
